1b7de968433d6cf00b6f2e9b3151ab5c5ac8902c
toan nguyen <avatar1341991@gmail.com>
2018-04-24 17:31:45 +0700

    ssssaa

---
+++ TeduShop.Model/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Model
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int ID { set; get; }
+
+    }
+}
